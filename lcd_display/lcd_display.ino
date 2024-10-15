5b722f9cf87fd42848a97c56c7dc1f1d5aefab0f
keni <kenitafili@gmail.com>
2024-10-15 18:06:32 +0200

    Revert "Add Potentiometer code"
    This reverts commit e9ffcab6f582a5edc42a1933ae9db461f7fccce5.

--- lcd_display/lcd_display.ino
+++ lcd_display/lcd_display.ino
@@ -1,3 +1,4 @@
+
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 
@@ -10,11 +11,6 @@ const int LEDS[] = {8, 9, 10, 11};
 long prevts[4];
 bool ledStates[] = {LOW, LOW, LOW, LOW};
 
-//variables needed for level selector (potentiometer)
-int currentValue;
-int difficulty;
-int newValue;
-
 void setup() {
   // put your setup code here, to run once:
   lcd.init();
@@ -23,7 +19,6 @@ void setup() {
   randomSeed(analogRead(0));
   int randomNum = random(16);
   lcd.print(randomNum, DEC);
-  currentValue = 0;
 
   for (int i = 0; i < 4; i++) {
     pinMode(BUTTONS[i], INPUT);
@@ -33,33 +28,9 @@ void setup() {
 }
 
 void loop() {
-  newValue = analogRead(A0);
-  if(newValue != currentValue){
-    currentValue = newValue;
-    if(currentValue >= 0 && currentValue <= 256 && difficulty != 1){
-      Serial.println("EASY :)");
-      difficulty = 1;
-    }else if(currentValue > 256 && currentValue <= 512 && difficulty != 2){
-      
-      Serial.println("MEDIUM ;)");
-      difficulty = 2;
-      
-    }else if(currentValue > 512 && currentValue <= 768 && difficulty != 3){
-      
-      Serial.println("HARD!");
-      difficulty = 3;
-      
-    }else if(currentValue > 768 && difficulty != 4){
-      Serial.println("EXPERT!!!");
-      difficulty = 4;
-    }
-    
-     
-  }
   for (int i = 0; i < 4; i++) {
     handle_button(BUTTONS[i], LEDS[i], i);
   }
-
 }
 
 
