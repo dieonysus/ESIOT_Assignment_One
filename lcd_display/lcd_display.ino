6b288e851ea1524c67c1e7530382728c7131b75f
IgorChukarin <Zigor2356@mail.ru>
2024-10-16 12:34:33 +0200

    now game works until the answer check moment

--- lcd_display/lcd_display.ino
+++ lcd_display/lcd_display.ino
@@ -1,4 +1,3 @@
-
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 
@@ -11,14 +10,22 @@ const int LEDS[] = {8, 9, 10, 11};
 long prevts[4];
 bool ledStates[] = {LOW, LOW, LOW, LOW};
 
+//variables needed for level selector (potentiometer)
+int currentValue;
+int newValue;
+int difficulty;
+
+bool isGameStarted = false;
+bool shouldDisplayNumber = true;
+
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
   lcd.init();
   lcd.backlight();
-  Serial.begin(9600);
   randomSeed(analogRead(0));
-  int randomNum = random(16);
-  lcd.print(randomNum, DEC);
+
+  currentValue = 0;
 
   for (int i = 0; i < 4; i++) {
     pinMode(BUTTONS[i], INPUT);
@@ -28,8 +35,50 @@ void setup() {
 }
 
 void loop() {
-  for (int i = 0; i < 4; i++) {
-    handle_button(BUTTONS[i], LEDS[i], i);
+  if (!isGameStarted) {
+    chooseDifficulty();
+    bool startButtonState = digitalRead(BUTTONS[0]);
+    if (startButtonState) {
+      isGameStarted = true;
+    }
+  } 
+  else {
+    if (shouldDisplayNumber) {
+      int randomNum = random(1, 16);
+      lcd.print(randomNum, DEC);
+      shouldDisplayNumber = false;
+    } else {
+      for (int i = 0; i < 4; i++) {
+        handle_button(BUTTONS[i], LEDS[i], i);
+      }
+    }
+  }
+
+}
+
+
+void chooseDifficulty() {
+    //read new potentiometer value from analog pin
+  newValue = analogRead(A1);
+  //compare if new value is different from the previous
+  if(newValue != currentValue){
+    //assign new value
+    currentValue = newValue;
+    //assigns the difficulty level according to potentiometer values
+    //if new difficulty is same as previous it DOES NOT print it again (&& difficulty != x)
+    if(currentValue >= 0 && currentValue <= 256 && difficulty != 1){
+      Serial.println("EASY :)");
+      difficulty = 1;
+    }else if(currentValue > 256 && currentValue <= 512 && difficulty != 2){
+      Serial.println("MEDIUM :|");
+      difficulty = 2;
+    }else if(currentValue > 512 && currentValue <= 768 && difficulty != 3){
+      Serial.println("HARD >:)");
+      difficulty = 3;
+    }else if(currentValue > 768 && difficulty != 4){
+      Serial.println("EXPERT >:D");
+      difficulty = 4;
+    }
   }
 }
 
